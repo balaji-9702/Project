50c451b8b3879168f13ab1cd11cd94bb80dbf59d
Balaji <tbalaji9702@gmail.com>
2022-09-14 15:33:19 +0530

    Exception handled

--- order_item.py
+++ order_item.py
@@ -8,23 +8,20 @@ iscomplete = False
 
 def list_of_items():
     
-    
+   #
+ try:  
     values = list(items.keys())
     print('LIST OF ITEMS')
     print(values)
     selected_items = input("> Enter the required items  from the list : ")
     split_items = list(selected_items.split(','))
     calac_amt(split_items)
-    
-
-
-    
-    add_items = input('Type add to add items / remove  to remove items from selected list  ').lower()
+    add_items = input('Type add to add items / remove  to remove items from selected list / Enter to finish  ').lower()
     if add_items == 'add':
        added_item=additional_item(split_items)
        split_items = split_items + added_item
        print(f'Purchased Items {split_items}')
-       print('----------------------------') 
+       print('-----------------------------------------') 
        calac_amt(split_items)
        """
     elif add_items == 'remove':
@@ -32,7 +29,10 @@ def list_of_items():
         """
 
     else:
-        print("Happy Shopping ! Welcome back :)")   
+        print("Happy Shopping ! Welcome back :)")
+
+ except TypeError:
+    print("Invalid value : Please Enter the proper value")     
       
    
 def additional_item(split_items):
